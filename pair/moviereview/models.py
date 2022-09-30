935c966bef71b8cf4e3532bafcef5c7f054ba1a9
최준혁 <fx887722@naver.com>
2022-09-30 10:55:34 +0900

    초기 설정, 모델 생성

--- pair/moviereview/models.py
+++ pair/moviereview/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    # 리뷰 제목
+    title = models.CharField(max_length=80)
+    # 리뷰 내용
+    content = models.TextField(null=True)
+    # 할 일 생성시간
+    created_at = models.DateTimeField(auto_now_add=True)
+    # 할 일 수정시간
+    updated_at = models.DateTimeField(auto_now=True)
